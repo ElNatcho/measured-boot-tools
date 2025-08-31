6db1d9e1d2dfbee343dbb952ca2d0df6049f8850
Sven Gebhard <sven.gebhard@fau.de>
2025-09-01 00:18:08 +0200

    rtmr.c: implemented rtmr_measure_tdhob, added evlog_add in rtmr_measure_cfv

--- calculate-tdx-mrs/rtmrms.c
+++ calculate-tdx-mrs/rtmrms.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause-Patent */
 
+#include "eventlog.h"
 #include <stdint.h> 
 
 #include <openssl/evp.h>
@@ -14,17 +15,31 @@ int rtmr_measure_tdhob(uint32_t mr_index, rtmrcontext_t *context)
 {
 	int ret = -1;
 
-	uint64_t len = get_td_hob_size();
-    uint8_t td_hob[len];
-    ret = create_td_hob(td_hob, len);
-    if (ret) {
-        return -1;
-    }
+	if (!context->ovmf_file_path) {
+		printf("Failed to open ovmf: no path provided\n");
+		return -1;
+	}
 
-	print_data_ext(td_hob, len, "Raw TD HOB:\n");
+	uint8_t* ovmf_buf = NULL;
+	uint64_t ovmf_size = 0;
+	ret = read_file(&ovmf_buf, &ovmf_size, context->ovmf_file_path);
+	if (ret) {
+		printf("Failed to load %s\n", context->ovmf_file_path);
+		return -1;
+	}
+
+	uint8_t* td_hob;
+	size_t td_hob_size;
+	create_td_hob_from_ovmf(&td_hob, &td_hob_size, ovmf_buf, ovmf_size);
+
+	//print_data_ext(td_hob, td_hob_size, "Raw TD HOB:");
 
     uint8_t hash_td_hob[SHA384_DIGEST_LENGTH];
-    hash_buf(EVP_sha384(), hash_td_hob, td_hob, len);
+    hash_buf(EVP_sha384(), hash_td_hob, td_hob, td_hob_size);
+
+	evlog_add(context->evlog, mr_index, "TD Hob", hash_td_hob,
+		   "TD Hob passed from host VMM to guest firmware");
+	hash_extend(EVP_sha384(), context->mrs[mr_index], hash_td_hob, SHA384_DIGEST_LENGTH);
 
 	return -1;
 }
@@ -34,7 +49,7 @@ int rtmr_measure_cfv(uint32_t mr_index, rtmrcontext_t *context)
 	int ret = -1;
 
 	if (!context->ovmf_file_path) {
-		printf("Failed to open OVMF.fd: no path provided\n");
+		printf("Failed to open ovmf: no path provided\n");
 		return -1;
 	}
 
@@ -52,7 +67,8 @@ int rtmr_measure_cfv(uint32_t mr_index, rtmrcontext_t *context)
 		printf("Failed to measure OVMF\n");
 	}
 
-	evlog_add(context->evlog, mr_index, "Configuration FV", hash_cfv, "Configuration Firmware Volume");
+	evlog_add(context->evlog, mr_index, "Configuration FV", hash_cfv,
+		   "Configuration Firmware Volume");
     
 	hash_extend(EVP_sha384(), context->mrs[mr_index], hash_cfv, SHA384_DIGEST_LENGTH);
 
